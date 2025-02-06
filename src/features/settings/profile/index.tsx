c24963bf67c2d3ee4288e8cba1ab73c2f5fb184c
Allan Souza <souzaallanxz@gmail.com>
2025-02-06 15:47:58 +0000

    changed profile description

--- src/features/settings/profile/index.tsx
+++ src/features/settings/profile/index.tsx
@@ -4,8 +4,8 @@ import { ProfileForm } from './profile-form'
 export default function SettingsProfile() {
   return (
     <ContentSection
-      title='Profile'
-      desc='This is how others will see you on the site.'
+      title='Perfil'
+      desc='Esta informação é usada para identificar o seu perfil na plataforma.'
     >
       <ProfileForm />
     </ContentSection>
