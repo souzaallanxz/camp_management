92817c69d7ac119346054605597deb7c4c933ecc
Allan Souza <souzaallanxz@gmail.com>
2025-02-26 22:19:55 +0000

    remove download button

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -101,7 +100,6 @@ export default function Dashboard() {
         <div className='mb-2 flex items-center justify-between space-y-2'>
           <h1 className='text-2xl font-bold tracking-tight'>Dashboard</h1>
           <div className='flex items-center space-x-2'>
-            <Button>Download</Button>
           </div>
         </div>
         <Tabs
